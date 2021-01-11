59118f9b01b93d9d7308b9fcb82682fffa170a24
Juan Manuel Pérez Toro <juanma.perez@autentia.com>
2021-01-11 11:52:31 +0000

    Added coins component and call to API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './components/Header/Header'
 import Search from './components/Search/Search'
+import Coins from './components/Coins'
+import getCoins from './services/CriptoService'
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+
+  const updateCriptoCoins = (value) => {
+    console.log("Update...")
+    console.log(value)
+  }
+
+  useEffect(async () => {
+    const response = await getCoins()
+
+    setCoins(response.data)
+  }, [])
+
   return (
     <div className="App">
       <Header />
-      <Search />
+      <Search 
+        updateCriptoCoins={updateCriptoCoins}
+      />
+      <Coins
+        coins={coins}
+      />
     </div>
   );
 }
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,20 @@
+import { computeHeadingLevel } from '@testing-library/react'
 import React from 'react'
 import style from './Search.css'
 
-const Search = () => (
-    <div className="search-container">
-        <input className="search" type="text" />
-    </div>
-)
+const Search = ({updateCriptoCoins}) => {
+
+    const handleValue = (e) => {
+        const criptoName = e.target.value
+        console.log("Handle value...")
+        updateCriptoCoins(criptoName)
+    }
+
+    return (
+        <div className="search-container">
+            <input className="search" type="text" onChange={handleValue}/>
+        </div>
+    )
+}
 
 export default Search
