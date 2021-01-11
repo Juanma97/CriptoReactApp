0e2be10fddebcbf82f80384dd9a85500cf7010ca
Juan Manuel Pérez Toro <juanma.perez@autentia.com>
2021-01-11 10:39:39 +0000

    Added search component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Header from './Header'
+import Header from './components/Header/Header'
+import Search from './components/Search/Search'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Search />
     </div>
   );
 }
---
+++ src/components/Search/Search.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './Search.css'
+
+const Search = () => (
+    <div className="search-container">
+        <input className="search" type="text" />
+    </div>
+)
+
+export default Search
