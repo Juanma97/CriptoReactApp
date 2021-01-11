81e2c804ff5c018bde6b7a66b01da0c98c186563
Juan Manuel Pérez Toro <juanma.perez@autentia.com>
2021-01-11 10:08:56 +0000

    Added header component and bebas font

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './Header.css'
+
+const Header = () => (
+    <div className="header">
+        <h1>Cotizador de criptomonedas</h1>
+    </div>
+)
+
+export default Header
